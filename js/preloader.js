c5cbd81c5cfce6028fd0a2dd57d180dd21b490fb
Aleksandr Kostiuk <kosskill2011@gmail.com>
2019-02-04 16:27:44 +0100

    addPreloader

---
+++ js/preloader.js
@@ -0,0 +1,12 @@
+document.body.onload = function() {
+
+    setTimeout(function() {
+        let preloader = document.getElementById('page-preloader');
+        if (!preloader.classList.contains('done') )
+    {
+        preloader.classList.add('done');
+    }
+    }, 2800);
+}
+
+
